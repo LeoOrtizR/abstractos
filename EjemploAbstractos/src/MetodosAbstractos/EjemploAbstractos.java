b46b59db3238bdd6a4f6eb3145e0bd51cb3b3ae5
Leonardo Ortiz Ramirez <lortizr@uniremingtonmanizales.edu.co>
2020-10-03 00:09:37 -0500

    Se instanciaron las Subclases en la Clase principal, y se llamaron los metodos abstractos que se definieron en la Super clase

--- EjemploAbstractos/src/MetodosAbstractos/EjemploAbstractos.java
+++ EjemploAbstractos/src/MetodosAbstractos/EjemploAbstractos.java
@@ -3,7 +3,14 @@ package MetodosAbstractos;
 public class EjemploAbstractos {
 
     public static void main(String[] args) {
+        Planta p = new Planta();
+        p.alimentar();
         
+        AnimalCarnivoro carnivoro = new AnimalCarnivoro();
+        carnivoro.alimentar();
+        
+        AnimalHerviboro herviboro = new AnimalHerviboro();
+        herviboro.alimentar();
     }
     
 }
