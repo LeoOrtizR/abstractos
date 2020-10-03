f8336857c160d21b3d2516579ca407e6c68acdee
Leonardo Ortiz Ramirez <lortizr@uniremingtonmanizales.edu.co>
2020-10-03 00:00:42 -0500

    Se creo la Subclase AnimalCarnivoro

---
+++ EjemploAbstractos/src/MetodosAbstractos/AnimalCarnivoro.java
@@ -0,0 +1,10 @@
+package MetodosAbstractos;
+
+public class AnimalCarnivoro extends Animal{
+    
+    @Override
+    public void alimentar(){
+        System.out.println("Los animales Carnivoros se alimentan de Carne");
+    }
+    
+}
