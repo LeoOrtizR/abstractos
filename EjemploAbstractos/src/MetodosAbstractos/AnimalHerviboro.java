1154446eb937b6e54f74d7c91c31606743fdfa74
Leonardo Ortiz Ramirez <lortizr@uniremingtonmanizales.edu.co>
2020-10-03 00:04:17 -0500

    Se creo la Subclase AnimalHerviboro

---
+++ EjemploAbstractos/src/MetodosAbstractos/AnimalHerviboro.java
@@ -0,0 +1,9 @@
+package MetodosAbstractos;
+
+public class AnimalHerviboro extends Animal{
+    
+    @Override
+    public void alimentar(){
+        System.out.println("Los Animales Herviboros se alimentan de Hierbas");
+    }
+}
