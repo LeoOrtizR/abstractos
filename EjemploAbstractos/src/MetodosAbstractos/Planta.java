38d7f3acd5222004a923ab14aa16c86037dc7cf5
Leonardo Ortiz Ramirez <lortizr@uniremingtonmanizales.edu.co>
2020-10-02 23:49:56 -0500

    Se creo la Subclase Planta

---
+++ EjemploAbstractos/src/MetodosAbstractos/Planta.java
@@ -0,0 +1,9 @@
+package MetodosAbstractos;
+
+public class Planta extends SerVivo{
+    
+    @Override
+    public void alimentar(){
+        System.out.println("Las plantas se alimentan mediante la Fotosintesis");
+    }
+}
